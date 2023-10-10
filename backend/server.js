e66e6854eb861de772764e1f5a41d6ee0b52a487
Maiko Yorino <122130876+maiko-y77@users.noreply.github.com>
2023-10-10 13:06:11 -0700

    Editing websocket (#10)
    * adding query to upsert database
    
    * Update ERD diagram
    
    * adding put request to create and update new appointmenti to server side
    
    * edditing cancelInterview function in frontend
    
    * adding delete query to database
    
    * eddited cancelInterview function
    
    * installed socket io
    
    * added socket.io feature
    
    * socket.io feature edit
    
    * comment TODO list
    
    * adding websoclet feature (not complete yet)
    
    * delete unnecessary comment
    
    * added websocket feature

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,99 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const dayRoutes = require("./routes/dayRoutes"); // import day router
-const cors = require('cors');
+const cors = require("cors");
 const { client } = require("./prisma");
 
-app.use(cors())
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`a client is connected, ${socket.id}`);
+    socket.on("updatedAppointments", (appointments) => {
+        socket.broadcast.emit("updatedAppointments", appointments);
+    });
+
+    socket.on("disconnection", () => {
+        console.log(`a client is disconnected, ${socket}`);
+    });
+});
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/", dayRoutes);
 
 app.get("/api/days/:day/interviewers", async (req, res) => {
-    const { day } = req.params
+    const { day } = req.params;
 
     const availableInterviewer = await client.availableInterviewer.findMany({
         where: { day: { name: day } },
         include: {
             interviewer: true
         }
-    })
+    });
 
-    const interviewers = availableInterviewer.map(interviewer => interviewer.interviewer)
-    res.send(interviewers)
-})
+    const interviewers = availableInterviewer.map(
+        (interviewer) => interviewer.interviewer
+    );
+    res.send(interviewers);
+});
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.get("/api/days/:day/appointments", async (req, res) => {
+    const { day } = req.params;
+
+    const appointments = await client.appointment.findMany({
+        where: { day: { name: day } },
+        include: { interview: { include: { interviewer: true } } },
+        orderBy: { id: "asc" }
+    });
+    res.send(appointments);
+});
+
+// Define an endpoint to handle interview upsert (create or update)
+
+app.put("/api/appointments/:id/interviews", async (req, res) => {
+    const { id } = req.params;
+    const appointment_id = parseInt(id);
+
+    const { student, interviewer_id } = req.body;
+    if (!student || !interviewer_id || !appointment_id) {
+        throw new Error(`Something went wrong need to be fixed!!!!!!!`);
+    }
+
+    try {
+        await client.interview.upsert({
+            create: { student, appointment_id, interviewer_id },
+            update: { student, appointment_id, interviewer_id },
+            where: {
+                appointment_id
+            }
+        });
+    } catch (err) {
+        throw new Error(`something went wrong!! while upserting!!`);
+    }
+    res.send({ message: "Successfully create or updated!!" });
+});
+
+app.delete("/api/interviews/:interviewId", async (req, res) => {
+    const { interviewId } = req.params;
+
+    try {
+        await client.interview.delete({
+            where: { id: parseInt(interviewId) }
+        });
+
+        res.status(204).send();
+    } catch (error) {
+        console.error("Error deleting interview:", error);
+        res.status(500).send("Something went wrong. Please try again.");
+    }
+});
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,84 +1,142 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 import "./App.scss";
+import { Socket, io } from "socket.io-client";
 
 import DayList from "./components/DayList";
 import Appointment from "./components/Appointment";
 
-import daysData from "./components/__mocks__/days.json";
-import appointmentsData from "./components/__mocks__/appointments.json";
+const socket = io("http://localhost:8000");
 
 export default function Application() {
   const [day, setDay] = useState("Monday");
   const [days, setDays] = useState([]);
   const [interviewers, setInterviewers] = useState([]);
-  const [appointments, setAppointments] = useState(appointmentsData);
+  const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/days')
+    axios
+      .get("http://localhost:8000/api/days")
       .then((res) => setDays(res.data))
-      .catch(error => console.log(`something went wrong in get request for days, ${error}`))
-  }, [])
+      .catch((error) =>
+        console.log(`something went wrong in get request for days, ${error}`)
+      );
+  }, []);
 
   useEffect(() => {
-    axios.get(`http://localhost:8000/api/days/${day}/interviewers`)
+    axios
+      .get(`http://localhost:8000/api/days/${day}/interviewers`)
       .then((res) => setInterviewers(res.data))
-      .catch(error => console.log(`something went wrong in get request for interviewers, ${error}`))
-  }, [day])
+      .catch((error) =>
+        console.log(
+          `something went wrong in get request for interviewers, ${error}`
+        )
+      );
+  }, [day]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/days/${day}/appointments`)
+      .then((res) => setAppointments(res.data))
+      .catch((error) =>
+        console.log(
+          `something went wrong in get request for appointments, ${error}`
+        )
+      );
+  }, [day]);
+
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("connect!!");
+    });
+
+    socket.on("disconnected", () => {
+      console.log("disconnected!!");
+    });
+
+    // update as needed, it is not good to send/receive the whole appointments
+    socket.on("updatedAppointments", (updatedAppointments) => {
+      setAppointments(updatedAppointments);
+      updateSpots(updatedAppointments)
+    });
+
+    return () => {
+      socket.disconnect();
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("updatedAppointments");
+    };
+  }, []);
+
+  function updateSpots(dayName, change) {
+    setDays((prev) => {
+      return prev.map((day) => {
+        if (day.name === dayName) {
+          return { ...day, spots: day.spots + change };
+        }
+        return day;
+      });
+    });
+  };
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
-    const isEdit = appointments[id].interview;
-    setAppointments((prev) => {
-      const appointment = {
-        ...prev[id],
-        interview: { ...interview },
-      };
-      const appointments = {
-        ...prev,
-        [id]: appointment,
-      };
-      return appointments;
+    const existingIndex = appointments.findIndex(
+      (appointment) => appointment.id === id
+    );
+    const isEditing = appointments[existingIndex].interview;
+
+    setAppointments((appointments) => {
+      const newAppointment = { ...appointments[existingIndex], interview };
+      const copied = [...appointments];
+      copied[existingIndex] = newAppointment;
+      console.log("appointment is updated")
+      socket.emit("updatedAppointments", copied);
+      return copied;
     });
-    if (!isEdit) {
-      setDays((prev) => {
-        const updatedDay = {
-          ...prev[day],
-          spots: prev[day].spots - 1,
-        };
-        const days = {
-          ...prev,
-          [day]: updatedDay,
-        };
-        return days;
+
+    axios
+      .put(`http://localhost:8000/api/appointments/${id}/interviews`, {
+        interviewer_id: interview.interviewer.id,
+        student: interview.student
+      })
+      .catch((err) => {
+        alert("something went wrong please reload your browser");
       });
+    if (!isEditing) {
+      updateSpots(day, -1);
     }
   }
+
   function cancelInterview(id) {
+    const findAppointmentsIndex = appointments.findIndex(
+      (appointments) => appointments.id === id
+    );
+
     setAppointments((prev) => {
       const updatedAppointment = {
-        ...prev[id],
-        interview: null,
+        ...prev[findAppointmentsIndex],
+        interview: null
       };
-      const appointments = {
-        ...prev,
-        [id]: updatedAppointment,
-      };
-      return appointments;
+      const copied = [...prev];
+      copied[findAppointmentsIndex] = updatedAppointment;
+      socket.emit("updatedAppointments", copied);
+      return copied;
     });
-    setDays((prev) => {
-      const updatedDay = {
-        ...prev[day],
-        spots: prev[day].spots + 1,
-      };
-      const days = {
-        ...prev,
-        [day]: updatedDay,
-      };
-      return days;
-    });
+
+    updateSpots(day, 1);
+
+    const interviewId = appointments[findAppointmentsIndex].interview.id;
+
+    axios
+      .delete(`http://localhost:8000/api/interviews/${interviewId}`)
+      .catch((err) => {
+        alert("something went wrong please reload");
+      });
   }
+
   return (
     <main className="layout">
       <section className="sidebar">
